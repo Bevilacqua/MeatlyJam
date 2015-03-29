f62c294a1e0fa50c4f4a07386a7ac27e6ae7255a
Jacob Bevilacqua <jacobbev@gmail.com>
2015-03-29 10:40:03 -0400

    Polish and submission

--- js/game.js
+++ js/game.js
@@ -1,5 +1,5 @@
 (function() {
-    var game = new Phaser.Game(1000 , 600 , Phaser.AUTO , '');
+    var game = new Phaser.Game(1000 , 600 , Phaser.AUTO , 'game');
     game.state.add('setup' , setup);
     game.state.add('main' , main)
     game.state.add('over' , over);
